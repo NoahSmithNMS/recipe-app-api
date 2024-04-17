c6c5f174b466e23012134e6d891a2f77b96f43bd
NoahSmith15 <Noah.Smith@pgip-tech.com>
2024-04-16 22:34:32 -0400

    migrations and first migrations

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,35 @@
+"""
+Database models.
+"""
 from django.db import models
+from django.contrib.auth.models import(
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_field):
+        #extra_field can provide keyword arguments
+        """Create save and return a new user"""
+        #self.model equivalent to User model as manager is associated to user class
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    #assigns user manager in django
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
